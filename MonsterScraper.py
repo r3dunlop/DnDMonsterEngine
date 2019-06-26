113dd22903ea91d3181071dde3adbd960ff25181
Ryan Dunlop <rdunlop@uoguelph.ca>
2019-06-25 20:55:09 -0400

    Major upgrades - Added exception catching for fields that may not exist - Added many new features for monster data - Added csv writing

--- MonsterScraper.py
+++ MonsterScraper.py
@@ -2,6 +2,9 @@ from bs4 import BeautifulSoup
 import requests
 import re
 import time
+import monster as mn
+import actions as ac 
+import csv
 
 def scrape_monster(monster_link):
     page_link = 'http://www.orcpub.com' + monster_link
@@ -9,68 +12,91 @@ def scrape_monster(monster_link):
     soup = BeautifulSoup(page_response.content, "html.parser")
     monster_name = soup.find_all('span',itemprop = 'name')[-1].get_text()
     
-    monster_info    = soup.find_all(class_ = 'char-details-section')
-    monster_size    = monster_info[0].find_all('em')[0].get_text()
-    monster_type    = monster_info[0].find_all('em')[1].get_text()
-    monster_align   = monster_info[0].find_all('em', class_='m-l-5')[1].get_text()
+    monster = mn.Monster(monster_name)
     
-    armor_class     = soup.find(string='Armor Class').find_parent(class_='char-details-field').find(class_='m-l-5').get_text()
-    hit_points      = soup.find(string='Hit Points').find_parent(class_='char-details-field').find(class_='m-l-5').get_text().split('(')[0]
-    hit_die         = '(' + soup.find(string='Hit Points').find_parent(class_='char-details-field').find(class_='m-l-5').get_text().split('(')[1]
-    speed           = soup.find(string='Speed').find_parent(class_='char-details-field').find(class_='m-l-5').get_text()
+    monster_info    = soup.find_all(class_ = 'char-details-section')
+    monster.size    = monster_info[0].find_all('em')[0].get_text()
+    monster.type    = monster_info[0].find_all('em')[1].get_text()
+    monster.align   = monster_info[0].find_all('em', class_='m-l-5')[1].get_text()
+    
+    monster.armor_class     = soup.find(string='Armor Class').find_parent(class_='char-details-field').find(class_='m-l-5').get_text()
+    monster.hp      = soup.find(string='Hit Points').find_parent(class_='char-details-field').find(class_='m-l-5').get_text().split('(')[0]
+    monster.hit_die         = '(' + soup.find(string='Hit Points').find_parent(class_='char-details-field').find(class_='m-l-5').get_text().split('(')[1]
+    monster.speed           = soup.find(string='Speed').find_parent(class_='char-details-field').find(class_='m-l-5').get_text()
     #str = soup.find_all('span')
-    ability_label_str = soup.find(string='str').find_parent(class_='ability-label').find('div').get_text()
-    ability_label_dex = soup.find(string='dex').find_parent(class_='ability-label').find('div').get_text()
-    ability_label_con = soup.find(string='con').find_parent(class_='ability-label').find('div').get_text()
-    ability_label_int = soup.find(string='int').find_parent(class_='ability-label').find('div').get_text()
-    ability_label_wis = soup.find(string='wis').find_parent(class_='ability-label').find('div').get_text()
-    ability_label_cha = soup.find(string='cha').find_parent(class_='ability-label').find('div').get_text()
+    monster.str = soup.find(string='str').find_parent(class_='ability-label').find('div').get_text()
+    monster.dex = soup.find(string='dex').find_parent(class_='ability-label').find('div').get_text()
+    monster.con = soup.find(string='con').find_parent(class_='ability-label').find('div').get_text()
+    monster.int = soup.find(string='int').find_parent(class_='ability-label').find('div').get_text()
+    monster.wis = soup.find(string='wis').find_parent(class_='ability-label').find('div').get_text()
+    monster.cha = soup.find(string='cha').find_parent(class_='ability-label').find('div').get_text()
 
-    proficiency_bonus = soup.find(string='Proficiency Bonus').find_parent(class_='char-details-field').find(class_='m-l-5').get_text()
+    monster.proficiency = soup.find(string='Proficiency Bonus').find_parent(class_='char-details-field').find(class_='m-l-5').get_text()
     #saving throws are redundant
-    skills = soup.find(string='Skills').find_parent(class_='char-details-field').find(class_='m-l-5').get_text()
-    senses = soup.find(string='Senses').find_parent(class_='char-details-field').find(class_='m-l-5').get_text()
-    languages = soup.find(string='Languages').find_parent(class_='char-details-field').find(class_='m-l-5').get_text()
-    challenge = soup.find(string='Challenge').find_parent(class_='char-details-field').find(class_='m-l-5').get_text()
-    extra_html = soup.find_all(class_='col-xs-12 col-md-6')[0].find_all('strong')
-    extras = ''
-    actions = ''
-    leg_actions = ''
-    for i in extra_html:
-        extras += i.get_text()
-    action_html = soup.find(string='Actions').find_parent('div').find_all('strong')
-    for i in action_html:
-        actions += i.get_text()
+    
+    try:
+        monster.skills = soup.find(string='Skills').find_parent(class_='char-details-field').find(class_='m-l-5').get_text()
+    except:
+        monster.skills = ''
+
+    try:
+        monster.resistances = soup.find(string='Damage Resistances').find_parent(class_='char-details-field').find(class_='m-l-5').get_text()
+    except:
+        monster.resistances = ''
+
+    try:
+        monster.immunities = soup.find(string='Damage Immunities').find_parent(class_='char-details-field').find(class_='m-l-5').get_text()
+    except:
+        monster.immunities = ''
+
+    try:
+        monster.cond_immunities = soup.find(string='Condition Immunities').find_parent(class_='char-details-field').find(class_='m-l-5').get_text()
+    except:
+        monster.cond_immunities = ''
+
+    try:
+        monster.senses = soup.find(string='Senses').find_parent(class_='char-details-field').find(class_='m-l-5').get_text()
+    except:
+        monster.senses = ''
+
+    try:
+        monster.languages = soup.find(string='Languages').find_parent(class_='char-details-field').find(class_='m-l-5').get_text()
+    except:
+        monster.languages = ''
+
+    monster.challenge = soup.find(string='Challenge').find_parent(class_='char-details-field').find(class_='m-l-5').get_text()
+
+    monster.extras = ''
+    monster.actions = ''
+    monster.leg_actions = ''
+
+    try:
+        extra_html = soup.find_all(class_='col-xs-12 col-md-6')[0].find_all('strong')
+        for i in extra_html:
+            monster.extras += i.get_text()
+        action_html = soup.find(string='Actions').find_parent('div').find_all('strong')
+    except:
+        monster.extras = ''
+
+    try:
+        action_html = soup.find(string='Actions').find_parent('div').find_all('strong')
+        for i in action_html:
+            monster.actions += i.get_text()
+    except:
+        monster.actions = ''
+
     try:
         leg_action_html = soup.find(string='Legendary Actions').find_parent('div').find_all('strong')
         for i in leg_action_html:
-            leg_actions += i.get_text()
+            monster.leg_actions += i.get_text()
     except:
-        leg_actions = ''
+        monster.leg_actions = ''
 
-    print(monster_name)
-    print(monster_size + ' ' + monster_type + ', ' + monster_align)
-    print('AC = ' + armor_class)
-    print('HP = ' + hit_points + ' ' + hit_die)
-    print('Speed = ' + speed)
-    print('str = ' + ability_label_str)
-    print('dex = ' + ability_label_dex)
-    print('con = ' + ability_label_con)
-    print('int = ' + ability_label_int)
-    print('wis = ' + ability_label_wis)
-    print('cha = ' + ability_label_cha)
-    print('proficiency bonus = ' + proficiency_bonus)
-    print('skills = ' + skills )
-    print('senses = ' + senses )
-    print('languages = ' + languages)
-    print('challenge = ' + challenge)
-    print('extras = ' + extras)
-    print('actions = ' + actions)
-    print('legendary = ' + leg_actions)
-    print('*********')
-
+    return monster
 
 page_link = 'http://www.orcpub.com/dungeons-and-dragons/5th-edition/monsters'
+output_filename = 'out_monsters.csv'
+csv_writer = csv.writer(open(output_filename,'w'))
 
 #Fetch the content from the url
 page_response = requests.get(page_link, timeout=5)
@@ -79,12 +105,18 @@ page_response = requests.get(page_link, timeout=5)
 soup = BeautifulSoup(page_response.content, "html.parser")
 textContent = []
 
+
 counter = 0
+
+csv_writer.writerow(mn.Monster.header())
 for link in soup.find_all('a'):
     if link.get('href') is not None and '/monsters/' in link.get('href'):
         #        print(link.get('href'))
-        scrape_monster(link.get('href'))
-        time.sleep(5) #Be nice to the website
+        monster = scrape_monster(link.get('href'))
+        monster.show()
+        csv_writer.writerow(monster.property_list())
+        
+        time.sleep(3) #Be nice to the website
 
 
 
